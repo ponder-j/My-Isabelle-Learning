875e5f283c207d909b18452e2b8fee0fca67e25e
ponder-j <a2140316092@qq.com>
2025-09-25 19:00:05 +0800

    Auto sync 2025-09-25 19:00:04

---
+++ Chapter_12/ListMonadTest.thy
@@ -0,0 +1,44 @@
+theory ListMonadTest
+    imports Main "HOL-Library.Monad_Syntax"
+begin
+
+(* List Monad *)
+(* 支持错误的计算结果（空列表）和多个成功的返回值 *)
+definition bind :: "'a list ⇒ ('a ⇒ 'b list) ⇒ 'b list"
+    where "bind xs f = concat (map f xs)"
+
+definition return :: "'a ⇒ 'a list"
+    where "return a ≡ [a]"
+
+(* 使用单子可以对列表的每个元素进行处理 *)
+definition "sqr_even l ≡
+  do {
+    x ← l;
+    if x mod 2 = 0 then
+        return (x * x)
+    else
+        return x
+  }"
+
+value "sqr_even [1..10]"
+
+(* 使用单子，可以很容易实现列表的复制 *)
+definition "list_double l ≡
+  do {
+    x ← l;
+    [x, 2 * x]
+  }"
+
+value "list_double [1..5]"
+
+(* 采用列表单子实现两个列表元素的任意组合 *)
+definition "prod1 xs ys ≡
+  do {
+    x ← xs;
+    y ← ys;
+    return (x, y)
+  }"
+
+value "prod1 [a,b,c] [e,f,g]"
+
+end
