8b15032dcd5dd201ce0b5db91a4aa94234e266bb
ponder-j <a2140316092@qq.com>
2025-09-26 17:00:05 +0800

    Auto sync 2025-09-26 17:00:04

--- Chapter_12/StateMonadTest.thy
+++ Chapter_12/StateMonadTest.thy
@@ -1,7 +1,9 @@
 theory StateMonadTest
-  imports Main "HOL-Library.Monad_Syntax"
+  imports Main "HOL-Library.State_Monad"
 begin
-
+(* 状态单子基本定义 *)
+datatype ('s, 'a) state = State (run_state: "'s ⇒ ('a × 's)")
+(* 状态单子定义为's ⇒ ('a × 's)的结构，把每个状态单子看作是一个改变状态并返回一个值的语句 *)
 
 
 end
