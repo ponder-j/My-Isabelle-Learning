27bd82c3ef4ce0accfc8c856a98d76575a8aa6cf
ponder-j <a2140316092@qq.com>
2025-09-26 18:00:05 +0800

    Auto sync 2025-09-26 18:00:05

--- Chapter_12/StateMonadTest.thy
+++ Chapter_12/StateMonadTest.thy
@@ -3,6 +3,11 @@ theory StateMonadTest
 begin
 (* 状态单子基本定义 *)
 datatype ('s, 'a) state = State (run_state: "'s ⇒ ('a × 's)")
+(* 带记录选择器的 datatype 定义，拆开来写： *)
+datatype ('s, 'a) state = State "'s ⇒ ('a × 's)"
+fun run_state :: "('s, 'a) state ⇒ 's ⇒ ('a × 's)" where
+  "run_state (State f) = f"
+
 (* 状态单子定义为's ⇒ ('a × 's)的结构，把每个状态单子看作是一个改变状态并返回一个值的语句 *)
 
 
