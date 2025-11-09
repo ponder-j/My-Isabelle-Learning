2ecb2da42140c3715a65ee0c37c40710dc3dcf2f
ponder-j <a2140316092@qq.com>
2025-11-09 18:00:04 +0800

    Auto update at 2025/11/09 18:00:04

---
+++ HSV/isabelle/2019/Task10_ConstantFolding.thy
@@ -0,0 +1,110 @@
+theory Task10_ConstantFolding
+imports Main
+begin
+
+(* Task 10 : Define a function that performs constant folding. Wherever it seesDefine a function that performs constant folding. Wherever it sees a gate with TRUE or FALSE as one of its inputs, it should try to replace the gate with TRUE, FALSE, or the other input, depending on the gate. *)
+
+(* Prove that constant folding is sound *)
+(* it never increases the area or delay of a circuit *)
+(* prove that if constant folding is applied to a circuit that has no inputs, the circuit thus obtained must be TRUE or FALSE *)
+
+datatype "circuit" = 
+  NOT "circuit"
+| AND "circuit" "circuit"
+| OR "circuit" "circuit"
+| TRUE
+| FALSE
+| INPUT "int"
+
+fun simulate where
+  "simulate (AND c1 c2) ρ = ((simulate c1 ρ) ∧ (simulate c2 ρ))"
+| "simulate (OR c1 c2) ρ = ((simulate c1 ρ) ∨ (simulate c2 ρ))"
+| "simulate (NOT c) ρ = (¬ (simulate c ρ))"
+| "simulate TRUE ρ = True"
+| "simulate FALSE ρ = False"
+| "simulate (INPUT i) ρ = ρ i"
+
+fun area :: "circuit ⇒ nat" where
+  "area (NOT c) = 1 + area c"
+| "area (AND c1 c2) = 1 + area c1 + area c2"
+| "area (OR c1 c2) = 1 + area c1 + area c2"
+| "area _ = 0"
+
+fun delay :: "circuit ⇒ nat" where
+  "delay (NOT c) = 1 + delay c"
+| "delay (AND c1 c2) = max (1 + delay c1) (1 + delay c2)"
+| "delay (OR c1 c2) = max (1 + delay c1) (1 + delay c2)"
+| "delay _ = 0"
+
+(* 错误示范1: 递归逻辑有误，无法实现化简后再化简，NOT (NOT TRUE) 只消去了一层 *)
+fun opt_CF_incorrect :: "circuit ⇒ circuit" where
+  "opt_CF_incorrect (NOT FALSE) = TRUE"
+| "opt_CF_incorrect (NOT TRUE) = FALSE"
+| "opt_CF_incorrect (NOT c) = NOT (opt_CF_incorrect c)"
+| "opt_CF_incorrect (AND FALSE c) = FALSE"
+| "opt_CF_incorrect (AND c FALSE) = FALSE"
+| "opt_CF_incorrect (AND TRUE c) = c"
+| "opt_CF_incorrect (AND c TRUE) = c"
+| "opt_CF_incorrect (OR c FALSE) = c"
+| "opt_CF_incorrect (OR FALSE c) = c"
+| "opt_CF_incorrect (OR TRUE c) = TRUE"
+| "opt_CF_incorrect (OR c TRUE) = TRUE"
+| "opt_CF_incorrect (AND c1 c2) = AND (opt_CF_incorrect c1) (opt_CF_incorrect c2)"
+| "opt_CF_incorrect (OR c1 c2) = OR (opt_CF_incorrect c1) (opt_CF_incorrect c2)"
+| "opt_CF_incorrect TRUE = TRUE"
+| "opt_CF_incorrect FALSE = FALSE"
+| "opt_CF_incorrect (INPUT i) = INPUT i"
+
+(* 错误示范2: 难以证明其 sound 性和两个 never_increase 性质 *)
+fun opt_CF_complicated :: "circuit ⇒ circuit" where
+  "opt_CF_complicated (NOT c) = (case opt_CF_complicated c of FALSE ⇒ TRUE | TRUE ⇒ FALSE | c' ⇒ NOT c')"
+| "opt_CF_complicated (AND c1 c2) = (let c1' = opt_CF_complicated c1; c2' = opt_CF_complicated c2 in
+    case (c1', c2') of
+      (FALSE, _) ⇒ FALSE
+    | (_, FALSE) ⇒ FALSE
+    | (TRUE, _) ⇒ c2'
+    | (_, TRUE) ⇒ c1'
+    | _ ⇒ AND c1' c2')"
+| "opt_CF_complicated (OR c1 c2) = (let c1' = opt_CF_complicated c1; c2' = opt_CF_complicated c2 in
+    case (c1', c2') of
+      (TRUE, _) ⇒ TRUE
+    | (_, TRUE) ⇒ TRUE
+    | (FALSE, _) ⇒ c2'
+    | (_, FALSE) ⇒ c1'
+    | _ ⇒ OR c1' c2')"
+| "opt_CF_complicated c = c"
+
+(* 参考答案的正确方法：将优化器拆成几个部件再合并，逻辑更清晰，证明也更容易 *)
+
+theorem opt_CF_is_sound : "simulate c ρ = simulate (opt_CF c) ρ"
+  apply (induct c rule: opt_CF.induct)
+  by auto
+
+theorem opt_CF_never_increase_area : "area c ≥ area (opt_CF c)"
+  apply (induct c rule: opt_CF.induct)
+  by auto
+
+theorem opt_CF_never_increase_delay : "delay c ≥ delay (opt_CF c)"
+  apply (induct c rule: opt_CF.induct)
+  by auto
+
+fun has_inputs :: "circuit ⇒ bool" where
+  "has_inputs (NOT c) = has_inputs c"
+| "has_inputs (AND c1 c2) = ((has_inputs c1) ∨ (has_inputs c2))"
+| "has_inputs (OR c1 c2) = ((has_inputs c1) ∨ (has_inputs c2))"
+| "has_inputs TRUE = False"
+| "has_inputs FALSE = False"
+| "has_inputs (INPUT i) = True"
+
+definition c1 :: "circuit" where
+  "c1 = (NOT (NOT (TRUE)))"
+
+value "has_inputs c1"
+value "opt_CF c1"
+
+
+theorem opt_CF_without_input_leads2_consistant : "¬ (has_inputs c) ⟹ (opt_CF c = TRUE ∨ opt_CF c = FALSE)"
+  apply (induct c rule: opt_CF.induct)
+  apply simp_all
+  apply fastforce+
+  done
