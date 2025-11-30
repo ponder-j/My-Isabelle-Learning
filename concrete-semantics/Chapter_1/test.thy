99536abd8240b47b9c44290a88f42406877317c3
ponder-j <a2140316092@qq.com>
2025-11-30 22:00:00 +0800

    Auto sync 2025-11-30 22:00:00

--- concrete-semantics/Chapter_1/test.thy
+++ concrete-semantics/Chapter_1/test.thy
@@ -2,6 +2,24 @@ theory test
   imports Main
 begin
 
+definition a :: nat where "a = 1"
+definition alist where "alist = [1,2,3]"
+
+value "(+) a a"
+
+value "map (\<lambda>x::nat. 2 * x) alist"
+
+fun add :: "nat => nat => nat" where
+  "add 0 n = n"
+|  "add (Suc m) n = Suc (add m n)"
+
+lemma add_02: "add m 0 = m"
+  apply (induction m)
+  apply simp+
+  done
+
+thm add_02
+
 
 
 end
